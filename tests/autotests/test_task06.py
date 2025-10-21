c86b5deb1cd940bafc382be6a12079c2f7b9cf90
Mikhail Gavrilenko <64466788+qrutyy@users.noreply.github.com>
2025-10-21 10:14:29 +0300

    Revert "Implement cfg to whnf and heelings based cfpq. Provide the solution w…"
    This reverts commit 480be63a21c9870c04de6c50ab3f9a094e9facbc.

--- tests/autotests/test_task06.py
+++ tests/autotests/test_task06.py
@@ -7,8 +7,9 @@ from grammars_constants import REGEXP_CFG, GRAMMARS
 from rpq_template_test import rpq_cfpq_test, different_grammars_test
 from cfpq_concrete_cases import CaseCFPQ, CASES_CFPQ
 
+# Fix import statements in try block to run tests
 try:
-    from project.t6_cfg_actions import hellings_based_cfpq
+    from project.task6 import hellings_based_cfpq
 except ImportError:
     pytestmark = pytest.mark.skip("Task 6 is not ready to test!")
 
@@ -29,8 +30,8 @@ class TestHellingBasedCFPQ:
 
 def test_cfg_to_weak_normal_form_exists():
     try:
-        import project.t6_cfg_actions
+        import project.task6
 
-        assert "cfg_to_weak_normal_form" in dir(project.t6_cfg_actions)
+        assert "cfg_to_weak_normal_form" in dir(project.task6)
     except NameError:
         assert False
