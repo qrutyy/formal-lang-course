a088a739426e417d04824fd0bf82a82e4dfe3f90
qrutyy <qrutyq@gmail.com>
2025-11-18 14:53:11 +0300

    tests: correct paths

--- tests/autotests/test_task08.py
+++ tests/autotests/test_task08.py
@@ -15,9 +15,9 @@ from cfpq_concrete_cases import CASES_CFPQ, CaseCFPQ
 
 # Fix import statements in try block to run tests
 try:
-    from project.task6 import hellings_based_cfpq
-    from project.task7 import matrix_based_cfpq
-    from project.task8 import tensor_based_cfpq, cfg_to_rsm, ebnf_to_rsm
+    from project.t6_cfg_actions import hellings_based_cfpq
+    from project.t7_matrix_cfpq import matrix_based_cfpq
+    from project.t8_tensor_re import tensor_based_cfpq, cfg_to_rsm, ebnf_to_rsm
 except ImportError:
     pytestmark = pytest.mark.skip("Task 8 is not ready to test!")
 
